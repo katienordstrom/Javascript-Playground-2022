46538f17bfcc5a16bf1dce4e6a80a485da8064a7
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-03-09 13:58:32 +1300

    added notes to app.js file

--- src/app.js
+++ src/app.js
@@ -2,3 +2,9 @@ console.log("Hello World")
 // TODO this is my to do list
 
 // NOTE this is my first note
+/*
+Today i created app.js in my own src folder. in order to create this i added node.js. 
+We learnt how to install things into our code from npm. 
+im not feeling the most confident with this so need to come back to it for revison. to create package.json we used npm init -y 
+-y is a flag for a default copy. 
+*/
