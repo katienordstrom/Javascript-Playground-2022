b24e316683bfa842423f39632ba357e92a4925d9
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-03-16 14:29:47 +1300

    Mid lesson break

--- src/week-4.js
+++ src/week-4.js
@@ -1,64 +1,141 @@
-//TODO 
-/* 
-Javascript Variables
-*/
-var fullName = "Katie Nordstrom"
-var classCode = 503
+// //TODO 
+// /* 
+// Javascript Variables
+// */
+// var fullName = "Katie Nordstrom"
+// var classCode = 503
 
-console.log(fullName)
-console.log(classCode)
+// console.log(fullName)
+// console.log(classCode)
 
-//Javascript Objects
-var full_name = 
-{
-first_name: "Katie",
-last_name: "Nordstrom",
-course_number:503
-}
-console.log(full_name)
-console.log(full_name.first_name)
-//NOTE stick with dot notation
-console.log(full_name["last_name"])
-//NOTE bracket notation works but requires brackets and quotations - more difficult.
+// //Javascript Objects
+// var full_name = 
+// {
+// first_name: "Katie",
+// last_name: "Nordstrom",
+// course_number:503
+// }
+// console.log(full_name)
+// console.log(full_name.first_name)
+// //NOTE stick with dot notation
+// console.log(full_name["last_name"])
+// //NOTE bracket notation works but requires brackets and quotations - more difficult.
 
-//JavaScript Functions -help us to write code more effectiently and reuse that block of code anywhere we want in our software files. IMproves readability of our code. 
+// //JavaScript Functions -help us to write code more effectiently and reuse that block of code anywhere we want in our software files. IMproves readability of our code. 
 
-function addTwoNumbers(num1, num2) {
-return num1+num2
-}
-console.log(addTwoNumbers(2, 3))
+// function addTwoNumbers(num1, num2) {
+// return num1+num2
+// }
+// console.log(addTwoNumbers(2, 3))
 
-function addThreeNumbers(num1, num2, num3) {
-    return num1+num2+num3
-}
+// function addThreeNumbers(num1, num2, num3) {
+//     return num1+num2+num3
+// }
 
-console.log(addThreeNumbers(2,5,7))
+// console.log(addThreeNumbers(2,5,7))
 
-//JavaScript Loops -there to be used to execute a certain part of code several times. Happens repeatedly -until the condition is completed. 
-for (var i=0; i<10; i++){
-console.log(i)
-}
+// //JavaScript Loops -there to be used to execute a certain part of code several times. Happens repeatedly -until the condition is completed. 
+// for (var i=0; i<10; i++){
+// console.log(i)
+// }
 
-// JavaScript if Statements - you can put conditions for a specific type of data you need - eg login credentials
-var number = 10
-if (number === 10) {
-    console.log("answer = " + number)
-} 
-else {
-    console.log("error")
-} 
-// === is strict quality. dont just just check the type of variable but check that it is 10! Otherwise JS might manipulate it and change something.
+// // JavaScript if Statements - you can put conditions for a specific type of data you need - eg login credentials
+// var number = 10
+// if (number === 10) {
+//     console.log("answer = " + number)
+// } 
+// else {
+//     console.log("error")
+// } 
+// // === is strict quality. dont just just check the type of variable but check that it is 10! Otherwise JS might manipulate it and change something.
 
-// const and let - var you can reassign the value-causes problems eg name, you could go back and change. 
-//if you dont want to change the data type of that  - const will keep this STRICLY thesame. 
+// // const and let - var you can reassign the value-causes problems eg name, you could go back and change. 
+// //if you dont want to change the data type of that  - const will keep this STRICLY thesame. 
 
-let num = 3 //swap let to const to see it stay strictly the same. 
-console.log(num)
+// let num = 3 //swap let to const to see it stay strictly the same. 
+// console.log(num)
 
 
-// num = "5" does not work bc youre reassigning. Says error. Cannot chnage the value PERIOD with const. 
-//Let allows a bit of flexibilty. You can use the number decalare in any other datatype. 
-//eg address - you can move house. const would not let you update it. let would allow you to update. 
+// /* num = "5" does not work bc youre reassigning. Says error. Cannot chnage the value PERIOD with const. 
+// Let allows a bit of flexibilty. You can use the number decalare in any other datatype. 
+// eg address - you can move house. const would not let you update it. let would allow you to update. */
+
+// //JavaScript Comment uses // 
+// /*This is a multiline coment*/
+
+// var sdv503 = "javascript"
+
+// var x = 3
+
+// var fullName =  "text"
+
+// // // foo(x,y)
+
+// // function foo(p1, p2){
+// // return p2-p1
+// // }
+// // foo(5,10)
+
+// // function sum(x1, x2){
+// //     return x1+x2
+// // }
+
+// // sum(10, 3)
+
+// /* Diff between (=) and (===)
+// single = asigns value to a variable. 
+// triple = used in conditional statements. When comparing two values. 
+// */
+
+// /* Statements vs Expressions
+// Statements is doing things. eg var numbers
+// Expressions produce values. eg var new_value = function sum (x1, x2)
+// */
+
+
+// //This is a statement
+// /*if (y >= 0) {
+//     x=y
+// } else {
+//     x=y
+// } 
+// */
+
+// //This is an Expression ES6
+// // var x = y >= 0 ? y : y
+
+// var word = "jhdiokling"
+// console.log(word.length)
+
+// //JavaScript Values (primitive vs objects)
+
+var number_one = 123
+var number_two = 123
+
+console.log(number_one === number_two)
+
+//These are primitave values in JS
+//Boolean: true and false
+//Numbers: 1234, 123.4
+//Strings: 'hdbsjhdj' or "jhshvjvs" either quotations is correct
+//Non-Values: null and undefined
+
+var str = "Nordstrom"
+console.log(str.length = 2)
+console.log(str.length)
+
+//Arrays
+
+var numbers = [1, 2, 3, 4, 5]
+console.log(numbers[3])
+
+
+
+
+
+
+
+
 
 
 
