a348f8eab31709ab5df3cc2b59b1c382fd5f97b6
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-03-30 11:43:22 +1300

    Lesson Challenge

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-console.log("Hello World")
+/*console.log("Hello World")
 console.dir(document)
 console.table({first:"test",val:9});
 console.error("Hello World")
@@ -8,5 +8,14 @@ var myCourse = "JavaScript";
 console.log(myName);
 alert("Welcome");
 var myAge = 23;
-console.log(myAge); 
+console.log(myAge); */
 //Adding my comment
+
+var welcomeMessage = "Kia Ora" //declaring variable to value Kia ora
+console.log(welcomeMessage)
+
+var myName = "Katie"
+console.log(myName)
+let myName = "Iain"
+
+
--- src/week-6.js
+++ src/week-6.js
@@ -5,13 +5,14 @@ var a = "hello"
 a
 console.log(a)*/
 
-window.alert("Hello World");
-alert("Hello World")
+//window.alert("Hello World");
+//alert("Hello World")
 //Both result in popup saying hello world
 
-var myName = "Katie";
-var myCourse = "JavaScript";
-console.log(myName);
-alert("Welcome");
-var myAge = 23;
-console.log(myAge); 
+var welcomeMessage = "Kia Ora" //declaring variable to value Kia ora
+console.log(welcomeMessage) //prints kia ora to the console
+
+var myName = "Katie" //declaring myName to the value of Katie
+console.log(myName) //prints Katie to the console
+var myName = "Iain" // redeclaring the value of myName to Iain
+console.log(myName) // prints iains name
