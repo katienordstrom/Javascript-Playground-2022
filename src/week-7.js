2a92ceddab7e50bb3cc9900da45ba70c7a41c1a1
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-04-06 11:32:31 +1200

    added fucntion to week-7

--- src/week-7.js
+++ src/week-7.js
@@ -3,13 +3,37 @@
 //Write a function to reverse and array
 // Example: reverseArray([1,2,3,4]) > [4,3,2,1]
 
-// const array1 = [1,2,3,4]
-// const reversed = array1.reverse()
-// console.log(reversed)
+// console.log("Hello World")
 
-console.log("Hello World")
+// function reverseArray(arr) {
+//     return arr.reverse()
+// }
+// console.log(reverseArray([1,2,3,4]))
 
-function reverseArray(arr) {
-    return arr.reverse()
+// Create a function that accepts an array and return the last element
+//Example: lastELement(["Katie", 23, "Purple"]) --try return purple
+
+// function lastElement(arr) {
+//     return arr.length-1
+  
+// }
+//   console.log(lastElement([1,2,3,4,5]))
+
+
+
+//Create a function that takes an array and returns it as a string
+
+// function tostringarray(arr) {
+
+//     return arr.toString()
+
+// }
+
+// const result = tostringarray(["ali", 2, 3, 4, "green"])
+// console.log(typeof result)
+
+function ArrayToString(arr){
+    return arr.join('')
+
 }
-console.log(reverseArray([1,2,3,4]))
+console.log(ArrayToString(["Katie ", 23, " Purple"]))
