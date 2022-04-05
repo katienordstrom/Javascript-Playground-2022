bedba46618cdf9a679c1acf548f0332be40629e5
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-04-06 10:54:43 +1200

    Update json

--- src/week-7.js
+++ src/week-7.js
@@ -1,2 +1,10 @@
 // WEEK 7
 
+//Write a function to reverse and array
+// Example: reverseArray([1,2,3,4]) > [4,3,2,1]
+
+// const array1 = [1,2,3,4]
+// const reversed = array1.reverse()
+// console.log(reversed)
+
+console.log("Hello World")
