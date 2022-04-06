2137c46cca40d8f69de2d061ab03efaa311d376a
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-04-06 13:45:07 +1200

    wtf

--- src/paircoding.js
+++ src/paircoding.js
@@ -1,7 +1,21 @@
 //Create a function that takes two strings and return either true or flase depending on whether
 //the total number of characters in the first string is equal to the total numbers of characters in the second string
 
-function nordstrom(name1, name2) {
-return name1.length === name2.length //=== strict on datatypes
+// function nordstrom(name1, name2) {
+// return name1.length === name2.length //=== strict on datatypes
+// }
+// console.log(nordstrom("AB", "CD")) //returns true, if you chnage to 3 letters=false
+
+//Write a function that takes an array of elements and returns the numbers in that array
+//and save them in a new array
+
+
+
+
+
+const onlyNumbers = arr => arr.filter(Numbers.isInterger) //ES6 to ES5
+
+function onlyNumbers(){burn
+
 }
-console.log(nordstrom("AB", "CD")) //returns true, if you chnage to 3 letters=false
+????????????????????? wtf
