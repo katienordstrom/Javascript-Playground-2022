e1ac646e3b48b6ab9efa621a6b7c3e47b16f010b
katienordstrom <katie-nordstrom@live.nmit.ac.nz>
2022-04-06 13:18:16 +1200

    added a new function

--- src/paircoding.js
+++ src/paircoding.js
@@ -1,2 +1,7 @@
 //Create a function that takes two strings and return either true or flase depending on whether
 //the total number of characters in the first string is equal to the total numbers of characters in the second string
+
+function nordstrom(name1, name2) {
+return name1.length === name2.length //=== strict on datatypes
+}
+console.log(nordstrom("AB", "CD")) //returns true, if you chnage to 3 letters=false
